f77371eed813b1991652529b4ee5c912b077ef3c
Sude Ceren OZDEN <sude.ceren.ozden@gmail.com>
2024-01-09 11:55:20 +0300

    satisfying requirement one

---
+++ hwk4/Program.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Program {
+
+  public static void main(String[] args) {
+  
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Please enter the first number: ");
+    int num1 = sc.nextInt();
+    /*from  w  ww  . j  a v  a2s  . c  om*/
+    System.out.print("Please enter the second number: ");
+    int num2 = sc.nextInt();
+    
+    for (int i= num1+1; i<num2; i++){
+        System.out.print(i+" ");
+    }
+    sc.close();
+ }
+    
+}
